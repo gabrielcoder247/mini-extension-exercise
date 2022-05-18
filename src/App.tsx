5bcc3358a9691b4223650a29a7d044de63e9392f
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-05-18 12:37:49 +0100

    Implement redux and display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Airtable from "airtable";
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Data, Form, setClasses, setMessage, setUser, setIsLoggedIn } from "./features";
+
+const base = new Airtable({ apiKey: "keyWqu5iHsEXi4Nz8"}).base("app8ZbcPx7dkpOnP0");
+
+const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state: Types.State) => state.app.isLoggedIn);
+
+  function logout() {
+    dispatch(setUser());
+    dispatch(setMessage());
+    dispatch(setClasses());
+    dispatch(setIsLoggedIn(false));
+  }
+
+  async function login(user: string, cb: () => void) {
+    dispatch(setMessage());
+    dispatch(setUser(user));
+    dispatch(setIsLoggedIn(false));
+
+    try {
+      const [student] = await base("Students")
+        .select({ filterByFormula: `({Name}="${user}")` })
+        .all();
+      console.log(student)
+      if (student == undefined) throw new Error(`${user} is not a user`);
+
+      const classes = await base("Classes")
+        .select({ filterByFormula: `OR(${(student.get("Classes") as string[])?.map((id) => `RECORD_ID()="${id}"`)})` })
+        .all();
+
+      const mates = await base("Students")
+        .select({
+          filterByFormula: `AND(
+            OR(${classes.map((cl) => `SEARCH("${cl.get("Name")}", {Classes})`)}),
+            NOT(RECORD_ID()="${student.id}")
+          )`,
+        })
+        .all();
+
+      dispatch(setIsLoggedIn(true));
+      dispatch(
+        setClasses(
+          classes.map((item) => ({
+            name: item.get("Name") as string,
+            students: (item.get("Students") as string[])
+              .map((student) => mates.find((mate) => mate.id === student)?.get("Name"))
+              .filter(Boolean) as string[],
+          }))
+        )
+      );
+      cb();
+    } catch (error) {
+      dispatch(setMessage((error as Record<string, string>).message));
+    }
+  }
+
+  return isLoggedIn ? <Data logout={logout} /> : <Form login={login} />;
+};
 
 export default App;
